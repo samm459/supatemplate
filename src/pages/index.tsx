8b35c8642f914752a32fd96c8e7e6048838e5bda
Sam Morey <sam@junogo.com>
2024-07-25 15:10:01 -0400

    Move pages to src

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <div className="border-b border-gray-200 p-4 text-xl">
+                supatemplate
+            </div>
+        </div>
+    )
+}
