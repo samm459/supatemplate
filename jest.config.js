3daa5440273bab849a23f5018748607a77bdd244
Sam Morey <sam@junogo.com>
2024-07-25 12:14:28 -0400

    Update jest config and ts config

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+  testEnvironment: "node",
+  transform: {
+    "^.+.tsx?$": ["ts-jest", {}],
+  },
+  setupFilesAfterEnv: ["<rootDir>/test/setup.ts"],
+  rootDir: "./",
+};
